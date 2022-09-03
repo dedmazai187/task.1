034a99477a8df2c63bce84829ac00c34ef31a96e
dedmazai187 <104206935+dedmazai187@users.noreply.github.com>
2022-09-03 16:41:02 +0300

    Add files via upload

---
+++ Task1.kt
@@ -0,0 +1,13 @@
+/* Вывести на экран большее из двух целых чисел. */
+
+fun main(){
+    val number1: Int = 12
+    val number2: Int =8
+
+    val max:Int = if(number1>number2)
+        number1
+    else
+        number2
+    println(max)
+//
+}
